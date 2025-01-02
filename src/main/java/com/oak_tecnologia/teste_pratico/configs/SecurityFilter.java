f06465c23f30e8e03c9fb3d8e91058542b3b1694
devopMarkz <marcosdev2002@gmail.com>
2025-01-02 18:51:55 -0300

    Implementacao de filtro para liberar o banco de dados H2

--- src/main/java/com/oak_tecnologia/teste_pratico/configs/SecurityFilter.java
+++ src/main/java/com/oak_tecnologia/teste_pratico/configs/SecurityFilter.java
@@ -1,12 +1,25 @@
 package com.oak_tecnologia.teste_pratico.configs;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityFilter {
+
+    @Bean
+    @Profile("test")
+    @Order(1)
+    public SecurityFilterChain h2SecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.securityMatcher(PathRequest.toH2Console()).csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+        return http.build();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
