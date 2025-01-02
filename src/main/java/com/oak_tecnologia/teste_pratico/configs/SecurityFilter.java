d75d4da59c17f022bd46d588ced8ec2d90cdc1b2
devopMarkz <marcosdev2002@gmail.com>
2025-01-02 18:31:52 -0300

    Criacao: Filtro de Seguranca para liberar acesso a endpoints

---
+++ src/main/java/com/oak_tecnologia/teste_pratico/configs/SecurityFilter.java
@@ -0,0 +1,18 @@
+package com.oak_tecnologia.teste_pratico.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityFilter {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .build();
+    }
+
+}
