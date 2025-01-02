36be8ad4890719017e7372e1676f5c62dbe7b0b1
devopMarkz <marcosdev2002@gmail.com>
2025-01-02 17:05:51 -0300

    Criacao: ProdutoService

---
+++ src/main/java/com/oak_tecnologia/backend/services/ProdutoService.java
@@ -0,0 +1,15 @@
+package com.oak_tecnologia.backend.services;
+
+import com.oak_tecnologia.backend.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+
+
+}
