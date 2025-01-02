1fd92abe3ebabf333cd81d0ba44ceff95369cd13
devopMarkz <marcosdev2002@gmail.com>
2025-01-02 17:33:23 -0300

    Criacao: ProdutoController

---
+++ src/main/java/com/oak_tecnologia/backend/controllers/ProdutoController.java
@@ -0,0 +1,14 @@
+package com.oak_tecnologia.backend.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+
+
+}
